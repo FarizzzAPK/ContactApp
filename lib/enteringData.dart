ce3c7a639010d245917972929aba5603e4630dab
FarizzzAPK <mahmoudfaris779@gmail.com>
2025-01-30 18:20:21 +0200

    Task Finished

--- lib/enteringData.dart
+++ lib/enteringData.dart
@@ -50,185 +50,189 @@ class _EnteringdataState extends State<Enteringdata> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0xff29384D),
+    return Padding(
+      padding:  MediaQuery.of(context).viewInsets,      child: Material(
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xff29384D),
 
-        ),
-        child: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    children: [
-                      if (_pickedImage != null)
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.file(
-                            _pickedImage!,
-                            height: 146,
-                            width: 146,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      if (_pickedImage == null)
-                        Container(
-                          height: 146,
-                          width: 146,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(28),
-                            border: Border.all(
-                              color: Color(0xffFFF1D4),
-                              width: 2,
-                            ),
-                          ),
-                          child: GestureDetector(
-                            onTap: _pickImageFromGallery,
-                            child: Lottie.asset(
-                              'assets/Lottie/image_picker.json',
+          ),
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        if (_pickedImage != null)
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.file(
+                              _pickedImage!,
+                              height: 146,
+                              width: 146,
                               fit: BoxFit.cover,
                             ),
                           ),
-                        ),
-                      SizedBox(width: 16),
-                      Container(
-                        height: 146,
-                        width: 200,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            ValueListenableBuilder(
-                              valueListenable: UserNameController,
-                              builder: (context, value, child) {
-                                return Text(
-                                  value.text.isEmpty
-                                      ? "User Name"
-                                      : value.text,
-                                  style: TextStyle(
-                                    color: Color(0xFFFDF6E3),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                    decoration: TextDecoration.none,
-                                  ),
-                                );
-                              },
-                            ),
-                            SizedBox(
-                              height: 32,
-                              child: Center(
-                                child: Container(
-                                  height: 1,
-                                  color: Color(0xffFFF1D4),
-                                  width: double.infinity,
-                                ),
+                        if (_pickedImage == null)
+                          Container(
+                            height: 146,
+                            width: 146,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(28),
+                              border: Border.all(
+                                color: Color(0xffFFF1D4),
+                                width: 2,
                               ),
                             ),
-                            ValueListenableBuilder(
-                              valueListenable: EmailController,
-                              builder: (context, value, child) {
-                                return Text(
-                                  value.text.isEmpty
-                                      ? "Email"
-                                      : value.text,
-                                  style: TextStyle(
-                                    color: Color(0xFFFDF6E3),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                    decoration: TextDecoration.none,
-                                  ),
-                                );
-                              },
-                            ),
-                            SizedBox(
-                              height: 32,
-                              child: Center(
-                                child: Container(
-                                  height: 1,
-                                  color: Color(0xffFFF1D4),
-                                  width: double.infinity,
-                                ),
+                            child: GestureDetector(
+                              onTap: _pickImageFromGallery,
+                              child: Lottie.asset(
+                                'assets/Lottie/image_picker.json',
+                                fit: BoxFit.cover,
                               ),
                             ),
-                            ValueListenableBuilder(
-                              valueListenable: PhoneNumberController,
-                              builder: (context, value, child) {
-                                return Text(
-                                  value.text.isEmpty
-                                      ? "+200000000000"
-                                      : value.text,
-                                  style: TextStyle(
-                                    color: Color(0xFFFDF6E3),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                    decoration: TextDecoration.none,
+                          ),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: Container(
+                            height: 146,
+                            width: 200,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ValueListenableBuilder(
+                                  valueListenable: UserNameController,
+                                  builder: (context, value, child) {
+                                    return Text(
+                                      value.text.isEmpty
+                                          ? "User Name"
+                                          : value.text,
+                                      style: TextStyle(
+                                        color: Color(0xFFFDF6E3),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600,
+                                        decoration: TextDecoration.none,
+                                      ),
+                                    );
+                                  },
+                                ),
+                                SizedBox(
+                                  height: 32,
+                                  child: Center(
+                                    child: Container(
+                                      height: 1,
+                                      color: Color(0xffFFF1D4),
+                                      width: double.infinity,
+                                    ),
                                   ),
-                                );
-                              },
+                                ),
+                                ValueListenableBuilder(
+                                  valueListenable: EmailController,
+                                  builder: (context, value, child) {
+                                    return Text(
+                                      value.text.isEmpty
+                                          ? "Email"
+                                          : value.text,
+                                      style: TextStyle(
+                                        color: Color(0xFFFDF6E3),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600,
+                                        decoration: TextDecoration.none,
+                                      ),
+                                    );
+                                  },
+                                ),
+                                SizedBox(
+                                  height: 32,
+                                  child: Center(
+                                    child: Container(
+                                      height: 1,
+                                      color: Color(0xffFFF1D4),
+                                      width: double.infinity,
+                                    ),
+                                  ),
+                                ),
+                                ValueListenableBuilder(
+                                  valueListenable: PhoneNumberController,
+                                  builder: (context, value, child) {
+                                    return Text(
+                                      value.text.isEmpty
+                                          ? "+200000000000"
+                                          : value.text,
+                                      style: TextStyle(
+                                        color: Color(0xFFFDF6E3),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600,
+                                        decoration: TextDecoration.none,
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  CustomInputField(
-                    textInputType: TextInputType.name,
-                    controller: UserNameController,
-                    hintText: "Enter User Name",
-                    validator: (value) =>
-                    value.isEmpty ? "Enter Your name" : null,
-                  ),
-                  SizedBox(height: 8),
-                  CustomInputField(
-                    textInputType: TextInputType.emailAddress,
-                    controller: EmailController,
-                    hintText: "Enter User Email",
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return "Enter User Email";
-                      } else if (!RegExp(
-                          r'^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
-                          .hasMatch(value)) {
-                        return "Enter valid email";
-                      }
-                    },
-                  ),
-                  SizedBox(height: 8),
-                  CustomInputField(
-                    textInputType: TextInputType.phone,
-                    controller: PhoneNumberController,
-                    hintText: "Enter User Phone",
-                    validator: (value) =>
-                    value.isEmpty ? "Enter User Phone" : null,
-                  ),
-                  SizedBox(height: 16),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        Color(0xffFFF1D4),
-                      ),
+                      ],
                     ),
-                    onPressed: () {
-                      newcontact();
-                      print(Contactmodel.Contacts);
+                    SizedBox(height: 16),
+                    CustomInputField(
+                      textInputType: TextInputType.name,
+                      controller: UserNameController,
+                      hintText: "Enter User Name",
+                      validator: (value) =>
+                      value.isEmpty ? "Enter Your name" : null,
+                    ),
+                    SizedBox(height: 8),
+                    CustomInputField(
+                      textInputType: TextInputType.emailAddress,
+                      controller: EmailController,
+                      hintText: "Enter User Email",
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Enter User Email";
+                        } else if (!RegExp(
+                            r'^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+                            .hasMatch(value)) {
+                          return "Enter valid email";
+                        }
+                      },
+                    ),
+                    SizedBox(height: 8),
+                    CustomInputField(
+                      textInputType: TextInputType.phone,
+                      controller: PhoneNumberController,
+                      hintText: "Enter User Phone",
+                      validator: (value) =>
+                      value.isEmpty ? "Enter User Phone" : null,
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color(0xffFFF1D4),
+                        ),
+                      ),
+                      onPressed: () {
+                        newcontact();
+                        print(Contactmodel.Contacts);
 
-                    },
-                    child: Text(
-                      "Enter user",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 20,
-                        color: Color(0xff29384D),
+                      },
+                      child: Text(
+                        "Enter user",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                          color: Color(0xff29384D),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
